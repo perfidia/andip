a54e4013aeb740d1b9dcccd7f0f78834f7a58432
alatar- <alatar@alatar.eu>
2013-03-13 00:26:12 +0100

    init wiki.py

--- src/andip/provider/wiki.py
+++ src/andip/provider/wiki.py
@@ -4,103 +4,29 @@ Created on Apr 27, 2012
 @author: Bartosz Alchimowicz
 '''
 
-import copy
-import collections
+import urllib
+import re
 
 from andip import DataProvider
 
 class WikiProvider(DataProvider):
     def __init__(self, url):
-        self.url = url
+        self.__url = url
         
-#    def _get_word(self, properties, data):
-#        assert len(data.keys()) == 1
-#
-#        key = data.keys()[0]
-#        val = properties[key]
-#        data = data[key]
-#
-#        if data.has_key(val):
-#            data = data[val]
-#        else:
-#            raise Exception("data not found, searched for = %s, available = %s" % (val, data))
-#
-#        del properties[key]
-#
-#        if isinstance(data, basestring):
-#            return data
-#
-#        if len(properties.keys()) == 0:
-#            #raise Exception("incorrect path: %s", data)
-#            return data
-#
-#        return self._get_word(properties, data)
-#
-#    def _build_cache_travel(self, data, past):
-#        if isinstance(data, basestring):
-#            tmp = (past[0], past[1], past[2])
-#
-#            if not self.conf_cache.has_key(data):
-#                self.conf_cache[data] = []
-#
-#            self.conf_cache[data].append(tmp)
-#
-#            return
-#
-#        assert len(data.keys()) == 1
-#
-#        k = data.keys()[0]
-#
-#        for v in data[k]:
-#            c = copy.deepcopy(past)
-#            c[2][k] = v
-#
-#            self._build_cache_travel(data[k][v], c)
-#
-#    def _build_cache(self):
-#        self.conf_cache = dict()
-#
-#        for part in self.data_set:
-#            for word in self.data_set[part]['word']:
-#                self._build_cache_travel(self.data_set[part]['word'][word], [part, word, {}])
-
-    def get_word(self, conf):
+    def _get_word(self, conf):
         assert isinstance(conf, tuple)
         assert len(conf) == 3
         assert isinstance(conf[0], basestring)
         assert isinstance(conf[1], basestring)
         assert isinstance(conf[2], dict)
+        #
 
-        part, base, properties = conf
-
-#        data = self.data_set[part]['word'][base]
-        
-#        switch = {
-#            'przymiotnik': 'one',
-#            'czasownik': 'two'
-#        }.get(properties[0], 'n/a')();
-
-
-#        if isinstance(data, dict):
-#            tmp = dict(properties) # copy, since _get_word removes fields
-#
-#            retval = self._get_word(tmp, data)
-#        else:
-#            retval = data
-#
-#        return retval
-
-    def get_conf(self, word):
+    def _get_conf(self, word):
         assert isinstance(word, str)
-
-#        if self.conf_cache is None:
-#            self._build_cache()
-#
-#        retval = self.conf_cache.get(word, None)
-#
-#        return retval
-    
-    def get_dump(self, word = None, conf = None):
+        
+        return urllib.urlopen(self.__url + 'w/api.php?format=xml&action=query&prop=revisions&rvprop=content&titles=' + word).read()
+        
+    def _get_dump(self, word = None, conf = None):
         """
         Dump data of a specified word in a string recognazible by FileProvider.
         @param word: a word to dump
@@ -111,6 +37,41 @@ class WikiProvider(DataProvider):
 
     
 class PlWikiProvider(WikiProvider):
+    
     def __init__(self):
-        WikiProvider.__init__(self, "http://pl.wiktionary.org/...")
+        WikiProvider.__init__(self, "http://pl.wiktionary.org/")
+    
+    def __get_conf_verb(self, data):
+        for conf in data:
+            config = dict()
+            conf = conf.replace("| ", "").split("\n")
+            for element in filter(None, conf): # filter removes empty elements
+                tmp = element.split("=")
+                config[tmp[0]] = tmp[1]
+            print config
+            # syf w kodowaniu
+                
+    def __get_conf_noun(self, data):
+        pass
+    def __get_conf_adjective(self, data):
+        pass
+    
+    def get_conf(self, word):
+        data = self._get_conf(word)
+
+        type = re.findall("-([^-]*)-polski", data)
+        if len(type) == 0:
+            raise Exception("word not found")
+        
+        return {
+            'przymiotnik': self.__get_conf_adjective, #
+            'czasownik': self.__get_conf_verb(re.findall("\{\{odmiana-czasownik-polski([^\}]*)}}", data)),
+            'rzeczownik': self.__get_conf_noun #
+        }.get(type[0]);
+
+    def get_word(self, conf):
+        return self._get_word(conf)
+
+    def get_dump(self, word = None, conf = None):
+        return self._get_dump(word, conf)
 
--- src/main.py
+++ src/main.py
@@ -7,28 +7,16 @@ from andip.provider.wiki import PlWikiProvider
 #ad = AnDiP(FileProvider("../data/polish"))
 ad = AnDiP(PlWikiProvider())
 
-print 'opisany', ad.get_word(('przymiotnik', 'opis', {'liczba': 'pojedyncza', 'rodzaj': 'm'}))
-print 'opisane', ad.get_word(('przymiotnik', 'opis', {'liczba': 'mnoga', 'rodzaj': 'm'}))
-print 'opisana', ad.get_word(('przymiotnik', 'opis', {'liczba': 'pojedyncza', 'rodzaj': 'ż'}))
-print 'opisane', ad.get_word(('przymiotnik', 'opis', {'liczba': 'mnoga', 'rodzaj': 'ż'}))
-
-print ad.get_word(("czasownik", "występować", {'aspekt': 'dokonane', 'forma': 'czas teraźniejszy', 'liczba': 'mnoga', 'osoba': 'trzecia'}))
-print ad.get_word(("czasownik", "występować", {'aspekt': 'dokonane', 'forma': 'czas przeszły', 'liczba': 'mnoga', 'osoba': 'trzecia'}))
-
-print ad.get_conf("wystąpił")
-
-
-print ad.get_word(('czasownik', 'być', {'forma': 'czas przesz\xc5\x82y', 'osoba': 'trzecia', 'aspekt': 'dokonane', 'liczba': 'pojedyncza'}))
-
-
-#ad = AnDiP(FileProvider("../data/english"))
+#print 'opisany', ad.get_word(('przymiotnik', 'opis', {'liczba': 'pojedyncza', 'rodzaj': 'm'}))
+#print 'opisane', ad.get_word(('przymiotnik', 'opis', {'liczba': 'mnoga', 'rodzaj': 'm'}))
+#print 'opisana', ad.get_word(('przymiotnik', 'opis', {'liczba': 'pojedyncza', 'rodzaj': 'ż'}))
+#print 'opisane', ad.get_word(('przymiotnik', 'opis', {'liczba': 'mnoga', 'rodzaj': 'ż'}))
 #
-#print "am",  ad.get_word(('verb', 'be', {'number': 'singular', 'person': 'first'}))
-#print "are", ad.get_word(('verb', 'be', {'number': 'singular', 'person': 'second'}))
-#print "is",  ad.get_word(('verb', 'be', {'number': 'singular', 'person': 'third'}))
+#print ad.get_word(("czasownik", "występować", {'aspekt': 'dokonane', 'forma': 'czas teraźniejszy', 'liczba': 'mnoga', 'osoba': 'trzecia'}))
+#print ad.get_word(("czasownik", "występować", {'aspekt': 'dokonane', 'forma': 'czas przeszły', 'liczba': 'mnoga', 'osoba': 'trzecia'}))
 #
-#print "are", ad.get_word(('verb', 'be', {'number': 'plural', 'person': 'first'}))
-#print "are", ad.get_word(('verb', 'be', {'number': 'plural', 'person': 'second'}))
-#print "are", ad.get_word(('verb', 'be', {'number': 'plural', 'person': 'third'}))
+print ad.get_conf("występować")
+print ad.get_conf("srać")
 #
-#print ad.get_conf("are")
+#
+#print ad.get_word(('czasownik', 'być', {'forma': 'czas przesz\xc5\x82y', 'osoba': 'trzecia', 'aspekt': 'dokonane', 'liczba': 'pojedyncza'}))
