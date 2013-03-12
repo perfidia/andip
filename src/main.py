4551b1cbe346e5c4843b8703b442bf0fdffa69a0
alatar- <alatar@alatar.eu>
2013-03-12 18:57:25 +0100

    Added WikiProvider draft

--- src/main.py
+++ src/main.py
@@ -2,8 +2,10 @@
 
 from andip import AnDiP
 from andip.provider import FileProvider
+from andip.provider.wiki import PlWikiProvider
 
-ad = AnDiP(FileProvider("../data/polish"))
+#ad = AnDiP(FileProvider("../data/polish"))
+ad = AnDiP(PlWikiProvider())
 
 print 'opisany', ad.get_word(('przymiotnik', 'opis', {'liczba': 'pojedyncza', 'rodzaj': 'm'}))
 print 'opisane', ad.get_word(('przymiotnik', 'opis', {'liczba': 'mnoga', 'rodzaj': 'm'}))
@@ -19,14 +21,14 @@ print ad.get_conf("wystąpił")
 print ad.get_word(('czasownik', 'być', {'forma': 'czas przesz\xc5\x82y', 'osoba': 'trzecia', 'aspekt': 'dokonane', 'liczba': 'pojedyncza'}))
 
 
-ad = AnDiP(FileProvider("../data/english"))
-
-print "am",  ad.get_word(('verb', 'be', {'number': 'singular', 'person': 'first'}))
-print "are", ad.get_word(('verb', 'be', {'number': 'singular', 'person': 'second'}))
-print "is",  ad.get_word(('verb', 'be', {'number': 'singular', 'person': 'third'}))
-
-print "are", ad.get_word(('verb', 'be', {'number': 'plural', 'person': 'first'}))
-print "are", ad.get_word(('verb', 'be', {'number': 'plural', 'person': 'second'}))
-print "are", ad.get_word(('verb', 'be', {'number': 'plural', 'person': 'third'}))
-
-print ad.get_conf("are")
+#ad = AnDiP(FileProvider("../data/english"))
+#
+#print "am",  ad.get_word(('verb', 'be', {'number': 'singular', 'person': 'first'}))
+#print "are", ad.get_word(('verb', 'be', {'number': 'singular', 'person': 'second'}))
+#print "is",  ad.get_word(('verb', 'be', {'number': 'singular', 'person': 'third'}))
+#
+#print "are", ad.get_word(('verb', 'be', {'number': 'plural', 'person': 'first'}))
+#print "are", ad.get_word(('verb', 'be', {'number': 'plural', 'person': 'second'}))
+#print "are", ad.get_word(('verb', 'be', {'number': 'plural', 'person': 'third'}))
+#
+#print ad.get_conf("are")
