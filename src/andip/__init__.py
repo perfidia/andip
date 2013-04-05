2260a9678f1ec3cec5fa324fc02f6d0dc4f30c00
Bartosz Alchimowicz <bartosz.alchimowicz@cs.put.poznan.pl>
2013-04-05 19:08:40 +0200

    add close method

--- src/andip/__init__.py
+++ src/andip/__init__.py
@@ -7,69 +7,72 @@ Created on Apr 27, 2012
 '''
 
 class AnDiP(object):
-	def __init__(self, provider, backoff = None):
-		assert isinstance(provider, DataProvider)
+    def __init__(self, provider, backoff = None):
+        assert isinstance(provider, DataProvider)
 
-		self.__provider = provider
-		self.__backoff = backoff;
-		self.__unsaved_items = list()
+        self.__provider = provider
+        self.__backoff = backoff;
+        self.__unsaved_items = list()
 
-	def get_word(self, conf):
-		"""
-		Get a specified word from a dictionary.
+    def get_word(self, conf):
+        """
+        Get a specified word from a dictionary.
 
-		@param conf a tuple build from three parts: part of a speech, base word and dict of properties
+        @param conf a tuple build from three parts: part of a speech, base word and dict of properties
 
-		@return word in a specified configuration
-		"""
+        @return word in a specified configuration
+        """
 
-		try:
-			retval = self.__backoff.get_word(conf)
-		except Exception:
-			retval = self.__provider.get_word(conf)
-			
-		if isinstance(retval, tuple):
-			self.__unsaved_items.append(retval[1])
-			return retval[0]
-		else:
-			return retval
+        try:
+            retval = self.__backoff.get_word(conf)
+        except Exception:
+            retval = self.__provider.get_word(conf)
 
-	def get_conf(self, word):
-		"""
-		Get the configuration for a given word.
+        if isinstance(retval, tuple):
+            self.__unsaved_items.append(retval[1])
+            return retval[0]
+        else:
+            return retval
 
-		Each configuration is a tuple build from three parts:
-		- part of a speech
-		- base word
-		- dict of properties
+    def get_conf(self, word):
+        """
+        Get the configuration for a given word.
 
-		@return a list with configurations
-		"""
-		
-		try:
-			return self.__backoff.get_conf(word)
-		except Exception:
-			return self.__provider.get_conf(word)
-		
-	def save(self, items = None):
-		"""
-		Saves all unsaved items using all providers including backoff.
-		"""
-		if items == None:
-			self.__backoff.save(self.__unsaved_items)
-		else:
-			for item in items:
-				self.__provider.save(item)
-			
+        Each configuration is a tuple build from three parts:
+        - part of a speech
+        - base word
+        - dict of properties
+
+        @return a list with configurations
+        """
+
+        try:
+            return self.__backoff.get_conf(word)
+        except Exception:
+            return self.__provider.get_conf(word)
+
+    def save(self, items = None):
+        """
+        Saves all unsaved items using all providers including backoff.
+        """
+        if items == None:
+            self.__backoff.save(self.__unsaved_items)
+        else:
+            for item in items:
+                self.__provider.save(item)
+
+    def close(self):
+        self.__provider.close()
 
 class DataProvider(object):
-	def get_word(self, conf):
-		raise Exception("abstract method")
+    def get_word(self, conf):
+        raise Exception("abstract method")
 
-	def get_conf(self, word):
-		raise Exception("abstract method")
-	
-	def save(self):
-		raise Exception("abstract method")
-	
-	
+    def get_conf(self, word):
+        raise Exception("abstract method")
+
+    def save(self):
+        raise Exception("abstract method")
+
+    def close(self):
+        pass
--- src/andip/provider/databaseprovider.py
+++ src/andip/provider/databaseprovider.py
@@ -6,26 +6,36 @@ from andip import DataProvider
 from andip.provider.database import Database
 
 class DatabaseProvider(DataProvider):
-    
     def __init__(self, url):
         self.database = Database(url)
-        
-    def close_database(self):
-        self.database.close()
-    
+
     def get_word(self, conf):
         '''
-            Returns word or throw KeyError, if there is no information
-            about word in database
+        Returns word or throw KeyError, if there is no information
+        about word in database
         '''
+        if not self.database:
+            raise IOError("Unable to access the database (closed?)")
+
         return self.database.get_word(conf[2], conf[1])
-    
+
     def get_conf(self, word):
         '''
-            Returns word configuration or KeyError, if there is no
-            information about word in database
+        Returns word configuration or KeyError, if there is no
+        information about word in database
         '''
+        if not self.database:
+            raise IOError("Unable to access the database (closed?)")
+
         return self.database.get_conf(word)
-    
+
     def save(self, conf):
+        if not self.database:
+            raise IOError("Unable to access the database (closed?)")
+
         self.database.save(conf[2], conf[1], conf[0])
+
+    def close(self):
+        self.database.close()
+
+        self.database = None
