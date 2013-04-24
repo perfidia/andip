3f7b8915ef2a8ec83b0154aaf5478046528c2dd3
alatar- <alatar@alatar.eu>
2013-04-24 17:50:14 +0200

    cleanup andip/__init__.py

--- src/andip/defaultprovider.py
+++ src/andip/defaultprovider.py
@@ -1,11 +1,43 @@
 # -*- coding: utf-8 -*-
 
 class DefaultProvider(object):
+
     def get_word(self, conf):
-        raise Exception("abstract method")
+        """
+        Get a specified word from a dictionary.
+
+        @param conf a tuple build from three parts:
+        - part of a speech,
+        - base word,
+        - dict of properties.
+
+        @return word in a specified configuration
+        """
+        raise NotImplementedError("abstract method")
 
     def get_conf(self, word):
-        raise Exception("abstract method")
+        """
+        Get the configuration for a given word.
 
-    def save(self):
-        raise Exception("abstract method")
+        Each configuration is a tuple build from three parts:
+        - part of a speech,
+        - base word,
+        - dict of properties.
+
+        @return a list with configurations
+        """
+        raise NotImplementedError("abstract method")
+
+    def test_configuration_format(self, conf):
+        """
+        Function test whether the configuration dictionary is build well.
+
+        Each configuration should be a tuple build from three parts:
+        - part of a speech,
+        - base word,
+        - dict of properties.
+
+        @return a list with configurations
+        """
+
+        raise AttributeError("Configuration test failed!")
