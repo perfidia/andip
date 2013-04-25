0966c527bad09f6fdaf70fa4786449314b2428d2
alatar- <alatar@alatar.eu>
2013-04-25 09:06:34 +0200

    fixed issue #18

--- src/andip/__init__.py
+++ src/andip/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
-from defaultprovider import DefaultProvider
-from fileprovider import FileProvider
-from databaseprovider import DatabaseProvider
-from wikiprovider import PlWikiProvider
+from default import DefaultProvider
+from file import FileProvider
+from database import DatabaseProvider
+from wiki import PlWikiProvider
 1
