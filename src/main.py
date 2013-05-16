85394c1e63445cdaa61d7178b97ee38cd658b11a
alatar- <alatar@alatar.eu>
2013-05-16 12:36:12 +0200

    fixed FileProvider errors

--- src/main.py
+++ src/main.py
@@ -4,40 +4,40 @@ from andip import *
 
 ad1 = PlWikiProvider()
 ad2 = DatabaseProvider("../data/polish", backoff = ad1)
-ad3 = FileProvider("../data/polish", backoff = ad2)
+ad3 = FileProvider("../data/polish")
 
-print ad3.get_word(('czasownik', 'być', {'aspekt': 'niedokonane', 'forma': 'czas terazniejszy', 'liczba': 'pojedyncza', 'osoba': 'trzecia'}))
-print ad3.get_word(("czasownik", "występować", {'aspekt': 'niedokonane', 'forma': 'czas terazniejszy', 'liczba': 'mnoga', 'osoba': 'trzecia'}))
-# print ad3.get_word(("czasownik", "robić", {'aspekt': 'niedokonane', 'forma': 'czas terazniejszy', 'liczba': 'mnoga', 'osoba': 'trzecia'}))
-# print ad3.get_word(("czasownik", "robić", {'aspekt': 'niedokonane', 'forma': 'czas przeszly', 'liczba': 'pojedyncza', 'osoba': 'trzecia', 'rodzaj' : 'meski'}))
-print ad3.get_word(("czasownik", "robić", {'aspekt': 'niedokonane', 'forma': 'czas terazniejszy', 'liczba': 'mnoga', 'osoba': 'trzecia'}))
-# print ad3.get_word(("czasownik", "robić", {'aspekt': 'niedokonane', 'forma': 'czas terazniejszy', 'liczba': 'mnoga', 'osoba': 'trzecia'}))
+print ad3.get_word(('czasownik', 'być', {'aspekt': 'niedokonane', 'forma': 'czas teraźniejszy', 'liczba': 'pojedyńcza', 'osoba': 'trzecia'}))
+print ad3.get_word(("czasownik", "występować", {'aspekt': 'niedokonane', 'forma': 'czas teraźniejszy', 'liczba': 'mnoga', 'osoba': 'trzecia'}))
+# print ad3.get_word(("czasownik", "robić", {'aspekt': 'niedokonane', 'forma': 'czas teraźniejszy', 'liczba': 'mnoga', 'osoba': 'trzecia'}))
+# print ad3.get_word(("czasownik", "robić", {'aspekt': 'niedokonane', 'forma': 'czas przeszły', 'liczba': 'pojedyńcza', 'osoba': 'trzecia', 'rodzaj' : 'm'}))
+# print ad3.get_word(("czasownik", "robić", {'aspekt': 'niedokonane', 'forma': 'czas teraźniejszy', 'liczba': 'mnoga', 'osoba': 'trzecia'}))
+# print ad3.get_word(("czasownik", "robić", {'aspekt': 'niedokonane', 'forma': 'czas teraźniejszy', 'liczba': 'mnoga', 'osoba': 'trzecia'}))
 # ad2.save_model(ad1.get_model())
-# print ad3.get_word(("czasownik", "występować", {'aspekt': 'niedokonane', 'forma': 'czas przeszly', 'liczba': 'pojedyncza', 'osoba': 'trzecia', 'rodzaj' : 'meski'}))
+print ad3.get_word(("czasownik", "występować", {'aspekt': 'niedokonane', 'forma': 'czas przeszły', 'liczba': 'pojedyńcza', 'osoba': 'trzecia', 'rodzaj' : 'm'}))
 # print ad3.get_word(('przymiotnik', 'żółty', {'przypadek' : 'dopełniacz', 'stopień' : 'wyższy', 'liczba': 'mnoga', 'rodzaj': 'm'}))
 # print ad3.get_word(('rzeczownik', 'pies', {'przypadek':'wołacz', 'liczba': 'mnoga'}));
 # print ad3.get_conf("żółtszym")                                            #
 # print ad3.get_conf('występują')                                           #
 # print ad3.get_conf('psie')
-# print ad3.get_word(("czasownik", "być", {'aspekt': 'niedokonane', 'forma': 'czas terazniejszy', 'liczba': 'mnoga', 'osoba': 'trzecia'}))
-# print ad3.get_word(('rzeczownik', 'pies', {'przypadek':'wołacz', 'liczba': 'pojedyncza'}));
-# print ad3.get_word(("czasownik", "występować", {'aspekt': 'niedokonane', 'forma': 'czas terazniejszy', 'liczba': 'mnoga', 'osoba': 'trzecia'}))
+print ad3.get_word(("czasownik", "być", {'aspekt': 'niedokonane', 'forma': 'czas teraźniejszy', 'liczba': 'mnoga', 'osoba': 'trzecia'}))
+# print ad3.get_word(('rzeczownik', 'pies', {'przypadek':'wołacz', 'liczba': 'pojedyńcza'}));
+print ad3.get_word(("czasownik", "występować", {'aspekt': 'niedokonane', 'forma': 'czas teraźniejszy', 'liczba': 'mnoga', 'osoba': 'trzecia'}))
 # print ad3.get_word(('przymiotnik', 'żółty', {'przypadek' : 'celownik', 'stopień' : 'wyższy', 'liczba': 'mnoga', 'rodzaj': 'm'}))
 # print ad3.get_word(('przymiotnik', 'żółty', {'przypadek' : 'dopełniacz', 'stopień' : 'najwyższy', 'liczba': 'mnoga', 'rodzaj': 'm'}))
 # print ad3.get_word(('przymiotnik', 'żółty', {'przypadek' : 'celownik', 'stopień' : 'najwyższy', 'liczba': 'mnoga', 'rodzaj': 'm'}))
-# print ad3.get_word(('czasownik', 'być', {'aspekt' : 'niedokonane', 'forma': 'czas terazniejszy', 'liczba': 'pojedyncza', 'osoba': 'trzecia'}))
-# print ad3.get_word(('czasownik', 'być', {'aspekt': 'niedokonane', 'forma': 'czas terazniejszy', 'liczba': 'mnoga', 'osoba': 'trzecia'}))
+print ad3.get_word(('czasownik', 'być', {'aspekt' : 'niedokonane', 'forma': 'czas teraźniejszy', 'liczba': 'pojedyńcza', 'osoba': 'trzecia'}))
+print ad3.get_word(('czasownik', 'być', {'aspekt': 'niedokonane', 'forma': 'czas teraźniejszy', 'liczba': 'mnoga', 'osoba': 'trzecia'}))
 
 
-ad2.save_model(ad1.get_model())
-print ad2.get_word(('czasownik', 'być', {'aspekt': 'niedokonane', 'forma': 'czas terazniejszy', 'liczba': 'pojedyncza', 'osoba': 'trzecia'}))
-print ad2.get_word(("czasownik", "występować", {'aspekt': 'niedokonane', 'forma': 'czas terazniejszy', 'liczba': 'mnoga', 'osoba': 'trzecia'}))
-
-print ad2.get_conf('robiłem')
-print ad2.get_conf('robię')
-print ad2.get_conf('robili')
-print ad2.get_conf('występowałam')
-print ad2.get_conf('występują')
+# ad2.save_model(ad1.get_model())
+# print ad2.get_word(('czasownik', 'być', {'aspekt': 'niedokonane', 'forma': 'czas teraźniejszy', 'liczba': 'pojedyńcza', 'osoba': 'trzecia'}))
+# print ad2.get_word(("czasownik", "występować", {'aspekt': 'niedokonane', 'forma': 'czas teraźniejszy', 'liczba': 'mnoga', 'osoba': 'trzecia'}))
+#
+# print ad2.get_conf('robiłem')
+# print ad2.get_conf('robię')
+# print ad2.get_conf('robili')
+# print ad2.get_conf('występowałam')
+# print ad2.get_conf('występują')
 
 ad2.close()
 ######################TESTY DLA GET_CONF###################################
@@ -48,9 +48,9 @@ ad2.close()
 #print ad.get_conf('najżółtszym')
 #print ad.get_conf('żółtszych')
 #print ad.get_conf('żółtszym')
-#print 'opisany', ad.get_word(('przymiotnik', 'opis', {'liczba': 'pojedyncza', 'rodzaj': 'm'}))
+#print 'opisany', ad.get_word(('przymiotnik', 'opis', {'liczba': 'pojedyńcza', 'rodzaj': 'm'}))
 #print 'opisane', ad.get_word(('przymiotnik', 'opis', {'liczba': 'mnoga', 'rodzaj': 'm'}))
-#print 'opisana', ad.get_word(('przymiotnik', 'opis', {'liczba': 'pojedyncza', 'rodzaj': 'ż'}))
+#print 'opisana', ad.get_word(('przymiotnik', 'opis', {'liczba': 'pojedyńcza', 'rodzaj': 'ż'}))
 #print 'opisane', ad.get_word(('przymiotnik', 'opis', {'liczba': 'mnoga', 'rodzaj': 'ż'}))
 #
 #print ad.get_word(("czasownik", "występować", {'aspekt': 'dokonane', 'forma': 'czas przeszły', 'liczba': 'mnoga', 'osoba': 'trzecia'}))
@@ -58,19 +58,19 @@ ad2.close()
 #print ad.get_word("występować")
 
 #db = DatabaseProvider.DatabaseProvider('Data')
-#print db.get_verb({'aspekt' : 'niedokonane', 'forma' : 'czas terazniejszy', 'liczba' : 'mnoga', 'osoba': 'trzecia'}, 'występować')
-#print db.get_verb({'aspekt' : 'niedokonane', 'forma' : 'czas terazniejszy', 'liczba' : 'mnoga', 'osoba': 'trzecia'}, 'zabierać')
+#print db.get_verb({'aspekt' : 'niedokonane', 'forma' : 'czas teraźniejszy', 'liczba' : 'mnoga', 'osoba': 'trzecia'}, 'występować')
+#print db.get_verb({'aspekt' : 'niedokonane', 'forma' : 'czas teraźniejszy', 'liczba' : 'mnoga', 'osoba': 'trzecia'}, 'zabierać')
 
 #print ad.get_conf('występować')
 
 #print ad.get_word(('przymiotnik', 'zachodni', {'stopień': 'podstawowy', 'przypadek' : 'dopełniacz', 'liczba': 'mnoga', 'rodzaj': 'm'}))
-#print ad.get_word(('czasownik', 'występować', {'aspekt' : 'niedokonane', 'forma' : 'czas terazniejszy', 'liczba' : 'mnoga', 'osoba': 'trzecia'}))
-#print ad.get_word(('czasownik', 'występować', {'aspekt' : 'niedokonane', 'forma' : 'czas terazniejszy', 'liczba' : 'pojedyncza', 'osoba': 'trzecia'}))
-#print ad.get_word(('czasownik', 'występować', {'aspekt' : 'niedokonane', 'forma' : 'czas terazniejszy', 'liczba' : 'mnoga', 'osoba': 'pierwsza'}))
-#print ad.get_word(('czasownik', 'występować', {'aspekt' : 'niedokonane', 'forma' : 'czas terazniejszy', 'liczba' : 'mnoga', 'osoba': 'druga'}))
-#print ad.get_word(('czasownik', 'występować', {'aspekt' : 'niedokonane', 'forma' : 'czas terazniejszy', 'liczba' : 'pojedyncza', 'osoba': 'druga'}))
+#print ad.get_word(('czasownik', 'występować', {'aspekt' : 'niedokonane', 'forma' : 'czas teraźniejszy', 'liczba' : 'mnoga', 'osoba': 'trzecia'}))
+#print ad.get_word(('czasownik', 'występować', {'aspekt' : 'niedokonane', 'forma' : 'czas teraźniejszy', 'liczba' : 'pojedyńcza', 'osoba': 'trzecia'}))
+#print ad.get_word(('czasownik', 'występować', {'aspekt' : 'niedokonane', 'forma' : 'czas teraźniejszy', 'liczba' : 'mnoga', 'osoba': 'pierwsza'}))
+#print ad.get_word(('czasownik', 'występować', {'aspekt' : 'niedokonane', 'forma' : 'czas teraźniejszy', 'liczba' : 'mnoga', 'osoba': 'druga'}))
+#print ad.get_word(('czasownik', 'występować', {'aspekt' : 'niedokonane', 'forma' : 'czas teraźniejszy', 'liczba' : 'pojedyńcza', 'osoba': 'druga'}))
 
-#print ad.get_word(('czasownik', 'występować', {'aspekt' : 'niedokonane', 'forma' : 'czas terazniejszy', 'liczba' : 'mnoga', 'osoba': 'trzecia'}))
+#print ad.get_word(('czasownik', 'występować', {'aspekt' : 'niedokonane', 'forma' : 'czas teraźniejszy', 'liczba' : 'mnoga', 'osoba': 'trzecia'}))
 #print ad.get_conf("występować")
 #print ad.get_conf("piękny")
 #print ad.get_conf("zdrowy")
@@ -79,4 +79,4 @@ ad2.close()
 #print ad.get_conf('kot')
 
 #
-#print ad.get_word(('czasownik', 'być', {'forma': 'czas przesz\xc5\x82y', 'osoba': 'trzecia', 'aspekt': 'dokonane', 'liczba': 'pojedyncza'}))
+#print ad.get_word(('czasownik', 'być', {'forma': 'czas przesz\xc5\x82y', 'osoba': 'trzecia', 'aspekt': 'dokonane', 'liczba': 'pojedyńcza'}))
--- tests/file/polish.py
+++ tests/file/polish.py
@@ -21,81 +21,81 @@ class FilePolishTest(unittest.TestCase):
         cls.ad = FileProvider(os.sep.join(path))
 
     def testGetWordPronoun(self):
-        self.assertEqual('niej', self.ad.get_word(('zaimek', 'ja', {'przypadek': 'miejscownik', 'liczba': 'pojedyncza', 'osoba': 'trzecia', 'rodzaj': 'ż'})))
-        self.assertEqual('nim',  self.ad.get_word(('zaimek', 'ja', {'przypadek': 'miejscownik', 'liczba': 'pojedyncza', 'osoba': 'trzecia', 'rodzaj': 'm'})))
+        self.assertEqual('niej', self.ad.get_word(('zaimek', 'ja', {'przypadek': 'miejscownik', 'liczba': 'pojedyńcza', 'osoba': 'trzecia', 'rodzaj': 'ż'})))
+        self.assertEqual('nim',  self.ad.get_word(('zaimek', 'ja', {'przypadek': 'miejscownik', 'liczba': 'pojedyńcza', 'osoba': 'trzecia', 'rodzaj': 'm'})))
         self.assertEqual('nich', self.ad.get_word(('zaimek', 'ja', {'przypadek': 'miejscownik', 'liczba': 'mnoga', 'osoba': 'trzecia', 'rodzaj': 'm'})))
 
-        self.assertEqual('on',   self.ad.get_word(('zaimek', 'ja', {'przypadek': 'mianownik', 'liczba': 'pojedyncza', 'osoba': 'trzecia', 'rodzaj': 'm'})))
-        self.assertEqual('ona',  self.ad.get_word(('zaimek', 'ja', {'przypadek': 'mianownik', 'liczba': 'pojedyncza', 'osoba': 'trzecia', 'rodzaj': 'ż'})))
+        self.assertEqual('on',   self.ad.get_word(('zaimek', 'ja', {'przypadek': 'mianownik', 'liczba': 'pojedyńcza', 'osoba': 'trzecia', 'rodzaj': 'm'})))
+        self.assertEqual('ona',  self.ad.get_word(('zaimek', 'ja', {'przypadek': 'mianownik', 'liczba': 'pojedyńcza', 'osoba': 'trzecia', 'rodzaj': 'ż'})))
 
-        self.assertEqual('której',  self.ad.get_word(('zaimek', 'który', {'przypadek': 'miejscownik', 'liczba': 'pojedyncza', 'rodzaj': 'ż'})))
-        self.assertEqual('którym',  self.ad.get_word(('zaimek', 'który', {'przypadek': 'miejscownik', 'liczba': 'pojedyncza', 'rodzaj': 'mos'})))
+        self.assertEqual('której',  self.ad.get_word(('zaimek', 'który', {'przypadek': 'miejscownik', 'liczba': 'pojedyńcza', 'rodzaj': 'ż'})))
+        self.assertEqual('którym',  self.ad.get_word(('zaimek', 'który', {'przypadek': 'miejscownik', 'liczba': 'pojedyńcza', 'rodzaj': 'mos'})))
         self.assertEqual('których', self.ad.get_word(('zaimek', 'który', {'przypadek': 'miejscownik', 'liczba': 'mnoga', 'rodzaj': 'mos'})))
 
     def testGetWordVerb(self):
-        self.assertEqual('następuje', self.ad.get_word(('czasownik', 'następować', {'aspekt': 'dokonane', 'forma': 'czas teraźniejszy', 'liczba': 'pojedyncza', 'osoba': 'pierwsza'})))
+        self.assertEqual('następuje', self.ad.get_word(('czasownik', 'następować', {'aspekt': 'dokonane', 'forma': 'czas teraźniejszy', 'liczba': 'pojedyńcza', 'osoba': 'pierwsza'})))
         self.assertEqual('następują', self.ad.get_word(('czasownik', 'następować', {'aspekt': 'dokonane', 'forma': 'czas teraźniejszy', 'liczba': 'mnoga', 'osoba': 'trzecia'})))
 
-        self.assertEqual('występuje', self.ad.get_word(('czasownik', 'występować', {'aspekt': 'dokonane', 'forma': 'czas teraźniejszy', 'liczba': 'pojedyncza', 'osoba': 'pierwsza'})))
-        self.assertEqual('występują', self.ad.get_word(('czasownik', 'występować', {'aspekt': 'dokonane', 'forma': 'czas teraźniejszy', 'liczba': 'mnoga', 'osoba': 'trzecia'})))
+        self.assertEqual('występuje', self.ad.get_word(('czasownik', 'występować', {'aspekt': 'niedokonane', 'forma': 'czas teraźniejszy', 'liczba': 'pojedyńcza', 'osoba': 'pierwsza'})))
+        self.assertEqual('występują', self.ad.get_word(('czasownik', 'występować', {'aspekt': 'niedokonane', 'forma': 'czas teraźniejszy', 'liczba': 'mnoga', 'osoba': 'trzecia'})))
 
-        self.assertEqual('jest', self.ad.get_word(('czasownik', 'być', {'aspekt': 'dokonane', 'forma': 'czas teraźniejszy', 'liczba': 'pojedyncza', 'osoba': 'trzecia'})))
-        self.assertEqual('są',   self.ad.get_word(('czasownik', 'być', {'aspekt': 'dokonane', 'forma': 'czas teraźniejszy', 'liczba': 'mnoga', 'osoba': 'trzecia'})))
+        self.assertEqual('jest', self.ad.get_word(('czasownik', 'być', {'aspekt': 'niedokonane', 'forma': 'czas teraźniejszy', 'liczba': 'pojedyńcza', 'osoba': 'trzecia'})))
+        self.assertEqual('są',   self.ad.get_word(('czasownik', 'być', {'aspekt': 'niedokonane', 'forma': 'czas teraźniejszy', 'liczba': 'mnoga', 'osoba': 'trzecia'})))
 
     def testGetWordPreposition(self):
         self.assertEqual('z',  self.ad.get_word(('przyimek', 'z', {'forma': 'z'})))
         self.assertEqual('ze', self.ad.get_word(('przyimek', 'z', {'forma': 'ze'})))
 
     def testGetWordAdjective(self):
-        self.assertEqual('opisany', self.ad.get_word(('przymiotnik', 'opis', {'liczba': 'pojedyncza', 'rodzaj': 'm'})))
+        self.assertEqual('opisany', self.ad.get_word(('przymiotnik', 'opis', {'liczba': 'pojedyńcza', 'rodzaj': 'm'})))
         self.assertEqual('opisane', self.ad.get_word(('przymiotnik', 'opis', {'liczba': 'mnoga', 'rodzaj': 'm'})))
-        self.assertEqual('opisana', self.ad.get_word(('przymiotnik', 'opis', {'liczba': 'pojedyncza', 'rodzaj': 'ż'})))
+        self.assertEqual('opisana', self.ad.get_word(('przymiotnik', 'opis', {'liczba': 'pojedyńcza', 'rodzaj': 'ż'})))
         self.assertEqual('opisane', self.ad.get_word(('przymiotnik', 'opis', {'liczba': 'mnoga', 'rodzaj': 'ż'})))
 
     def testGetWordNoun(self):
-        self.assertEqual('znaku',  self.ad.get_word(('rzeczownik', 'znak', {'przypadek': 'dopełniacz', 'liczba': 'pojedyncza'})))
+        self.assertEqual('znaku',  self.ad.get_word(('rzeczownik', 'znak', {'przypadek': 'dopełniacz', 'liczba': 'pojedyńcza'})))
         self.assertEqual('znaków', self.ad.get_word(('rzeczownik', 'znak', {'przypadek': 'dopełniacz', 'liczba': 'mnoga'})))
 
-        self.assertEqual('znak',  self.ad.get_word(('rzeczownik', 'znak', {'przypadek': 'mianownik', 'liczba': 'pojedyncza'})))
+        self.assertEqual('znak',  self.ad.get_word(('rzeczownik', 'znak', {'przypadek': 'mianownik', 'liczba': 'pojedyńcza'})))
         self.assertEqual('znaki', self.ad.get_word(('rzeczownik', 'znak', {'przypadek': 'mianownik', 'liczba': 'mnoga'})))
 
     def testGetConfPronoun(self):
-        self.assertIn(('zaimek', 'ja', {'przypadek': 'miejscownik', 'liczba': 'pojedyncza', 'osoba': 'trzecia', 'rodzaj': 'ż'}), self.ad.get_conf('niej'))
-        self.assertIn(('zaimek', 'ja', {'przypadek': 'miejscownik', 'liczba': 'pojedyncza', 'osoba': 'trzecia', 'rodzaj': 'm'}),  self.ad.get_conf('nim'))
+        self.assertIn(('zaimek', 'ja', {'przypadek': 'miejscownik', 'liczba': 'pojedyńcza', 'osoba': 'trzecia', 'rodzaj': 'ż'}), self.ad.get_conf('niej'))
+        self.assertIn(('zaimek', 'ja', {'przypadek': 'miejscownik', 'liczba': 'pojedyńcza', 'osoba': 'trzecia', 'rodzaj': 'm'}),  self.ad.get_conf('nim'))
         self.assertIn(('zaimek', 'ja', {'przypadek': 'miejscownik', 'liczba': 'mnoga', 'osoba': 'trzecia', 'rodzaj': 'm'}), self.ad.get_conf('nich'))
 
-        self.assertEqual(('zaimek', 'ja', {'przypadek': 'mianownik', 'liczba': 'pojedyncza', 'osoba': 'trzecia', 'rodzaj': 'm'}),  self.ad.get_conf('on')[0])
-        self.assertEqual(('zaimek', 'ja', {'przypadek': 'mianownik', 'liczba': 'pojedyncza', 'osoba': 'trzecia', 'rodzaj': 'ż'}), self.ad.get_conf('ona')[0])
+        self.assertEqual(('zaimek', 'ja', {'przypadek': 'mianownik', 'liczba': 'pojedyńcza', 'osoba': 'trzecia', 'rodzaj': 'm'}),  self.ad.get_conf('on')[0])
+        self.assertEqual(('zaimek', 'ja', {'przypadek': 'mianownik', 'liczba': 'pojedyńcza', 'osoba': 'trzecia', 'rodzaj': 'ż'}), self.ad.get_conf('ona')[0])
 
-        self.assertIn(('zaimek', 'który', {'przypadek': 'miejscownik', 'liczba': 'pojedyncza', 'rodzaj': 'ż'}),  self.ad.get_conf('której'))
-        self.assertIn(('zaimek', 'który', {'przypadek': 'miejscownik', 'liczba': 'pojedyncza', 'rodzaj': 'mos'}),  self.ad.get_conf('którym'))
+        self.assertIn(('zaimek', 'który', {'przypadek': 'miejscownik', 'liczba': 'pojedyńcza', 'rodzaj': 'ż'}),  self.ad.get_conf('której'))
+        self.assertIn(('zaimek', 'który', {'przypadek': 'miejscownik', 'liczba': 'pojedyńcza', 'rodzaj': 'mos'}),  self.ad.get_conf('którym'))
         self.assertIn(('zaimek', 'który', {'przypadek': 'miejscownik', 'liczba': 'mnoga', 'rodzaj': 'mos'}), self.ad.get_conf('których'))
 
     def testGetConfVerb(self):
-        self.assertIn(('czasownik', 'następować', {'aspekt': 'dokonane', 'forma': 'czas teraźniejszy', 'liczba': 'pojedyncza', 'osoba': 'pierwsza'}), self.ad.get_conf('następuje'))
+        self.assertIn(('czasownik', 'następować', {'aspekt': 'dokonane', 'forma': 'czas teraźniejszy', 'liczba': 'pojedyńcza', 'osoba': 'pierwsza'}), self.ad.get_conf('następuje'))
         self.assertEqual(('czasownik', 'następować', {'aspekt': 'dokonane', 'forma': 'czas teraźniejszy', 'liczba': 'mnoga', 'osoba': 'trzecia'}), self.ad.get_conf('następują')[0])
 
-        self.assertIn(('czasownik', 'występować', {'aspekt': 'dokonane', 'forma': 'czas teraźniejszy', 'liczba': 'pojedyncza', 'osoba': 'pierwsza'}), self.ad.get_conf('występuje'))
-        self.assertEqual(('czasownik', 'występować', {'aspekt': 'dokonane', 'forma': 'czas teraźniejszy', 'liczba': 'mnoga', 'osoba': 'trzecia'}), self.ad.get_conf('występują')[0])
+        self.assertIn(('czasownik', 'występować', {'aspekt': 'niedokonane', 'forma': 'czas teraźniejszy', 'liczba': 'pojedyńcza', 'osoba': 'pierwsza'}), self.ad.get_conf('występuje'))
+        self.assertEqual(('czasownik', 'występować', {'aspekt': 'niedokonane', 'forma': 'czas teraźniejszy', 'liczba': 'mnoga', 'osoba': 'trzecia'}), self.ad.get_conf('występują')[0])
 
-        self.assertEqual(('czasownik', 'być', {'aspekt': 'dokonane', 'forma': 'czas teraźniejszy', 'liczba': 'pojedyncza', 'osoba': 'trzecia'}), self.ad.get_conf('jest')[0])
-        self.assertEqual(('czasownik', 'być', {'aspekt': 'dokonane', 'forma': 'czas teraźniejszy', 'liczba': 'mnoga', 'osoba': 'trzecia'}),   self.ad.get_conf('są')[0])
+        self.assertEqual(('czasownik', 'być', {'aspekt': 'niedokonane', 'forma': 'czas teraźniejszy', 'liczba': 'pojedyńcza', 'osoba': 'trzecia'}), self.ad.get_conf('jest')[0])
+        self.assertEqual(('czasownik', 'być', {'aspekt': 'niedokonane', 'forma': 'czas teraźniejszy', 'liczba': 'mnoga', 'osoba': 'trzecia'}),   self.ad.get_conf('są')[0])
 
     def testGetConfPreposition(self):
         self.assertEqual(('przyimek', 'z', {'forma': 'z'}),  self.ad.get_conf('z')[0])
         self.assertEqual(('przyimek', 'z', {'forma': 'ze'}), self.ad.get_conf('ze')[0])
 
     def testGetConfAdjective(self):
-        self.assertEqual(('przymiotnik', 'opis', {'liczba': 'pojedyncza', 'rodzaj': 'm'}), self.ad.get_conf('opisany')[0])
+        self.assertEqual(('przymiotnik', 'opis', {'liczba': 'pojedyńcza', 'rodzaj': 'm'}), self.ad.get_conf('opisany')[0])
         self.assertIn(('przymiotnik', 'opis', {'liczba': 'mnoga', 'rodzaj': 'm'}), self.ad.get_conf('opisane'))
-        self.assertEqual(('przymiotnik', 'opis', {'liczba': 'pojedyncza', 'rodzaj': 'ż'}), self.ad.get_conf('opisana')[0])
+        self.assertEqual(('przymiotnik', 'opis', {'liczba': 'pojedyńcza', 'rodzaj': 'ż'}), self.ad.get_conf('opisana')[0])
         self.assertIn(('przymiotnik', 'opis', {'liczba': 'mnoga', 'rodzaj': 'ż'}), self.ad.get_conf('opisane'))
 
     def testGetConfNoun(self):
-        self.assertIn(('rzeczownik', 'znak', {'przypadek': 'dopełniacz', 'liczba': 'pojedyncza'}),  self.ad.get_conf('znaku'))
+        self.assertIn(('rzeczownik', 'znak', {'przypadek': 'dopełniacz', 'liczba': 'pojedyńcza'}),  self.ad.get_conf('znaku'))
         self.assertEqual(('rzeczownik', 'znak', {'przypadek': 'dopełniacz', 'liczba': 'mnoga'}), self.ad.get_conf('znaków')[0])
 
-        self.assertIn(('rzeczownik', 'znak', {'przypadek': 'mianownik', 'liczba': 'pojedyncza'}),  self.ad.get_conf('znak'))
+        self.assertIn(('rzeczownik', 'znak', {'przypadek': 'mianownik', 'liczba': 'pojedyńcza'}),  self.ad.get_conf('znak'))
         self.assertIn(('rzeczownik', 'znak', {'przypadek': 'mianownik', 'liczba': 'mnoga'}), self.ad.get_conf('znaki'))
 
 if __name__ == '__main__':
